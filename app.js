a7532e6a3787ea26ffe1abe095e364009fd9e705
kishan <kkwriting2018@gmail.com>
2020-06-14 12:27:24 +0530

    Synchronous Read file

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-const events = require('events');
-const util = require('util');
+var fs = require('fs');
 
-var Person = function (name) {
-  this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-let james = new Person('James');
-let mary = new Person('Mary');
-let ryu = new Person('Ryu');
-
-let people = [james, mary, ryu];
-
-people.forEach(function(person) {
-  person.on('speak', function(mssg) {
-    console.log(`${person.name} said: ${mssg}`)
-  }) 
-})
-
-james.emit('speak', 'hey dude')
-ryu.emit('speak', 'I want curry')
+var readMe = fs.readFileSync('readMe.txt', 'utf8');
+console.log(readMe)
+var writeMe = fs.writeFileSync('writeMe.txt', readMe);
