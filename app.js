7348cbd40382fa05b91ecaf153c1a9df58e8d156
kishan <kkwriting2018@gmail.com>
2020-06-15 17:47:39 +0530

    Server Created

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var fs = require('fs');
+var http = require('http')
 
-// Asynchronous
-// Runs in the background while the further code runs
-fs.readFile('readMe.txt', 'utf8', function(err, data) {
-  console.log(data)
-  fs.writeFile('writeMe.txt', data)
-})
+var server = http.createServer(function(req, res){
+  console.log('request was made: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hey Monk')
+});
 
-console.log('test');
+server.listen(3000, '127.0.0.1')
+console.log('Listening to port 3000')
