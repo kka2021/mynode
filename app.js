91d67b41068c28ff121631d2f1fdc9df18b0e4ba
kishan <kkwriting2018@gmail.com>
2020-06-18 14:08:55 +0530

    Template Engines

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express')
 
 var app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
@@ -10,8 +12,9 @@ app.get('/contact', function(req, res) {
   res.sendFile(__dirname + '/contact.html')
 })
 
-app.get('/profile/:id', function(req, res) {
-  res.send('You requested to see a profile with the id of ' + req.params.id)
+app.get('/profile/:name', function(req, res) {
+  var data = {age: 29, job: 'ninja'}
+  res.render('profile', {person: req.params.name, data: data})
 })
 
 app.listen(3000)
